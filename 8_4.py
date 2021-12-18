4293d395e7d0a59e4f1a6d28c4077379775e157c
tupator4y <shulginscrew@gmail.com>
2021-12-18 15:15:29 +0300

    les8

---
+++ 8_4.py
@@ -0,0 +1,25 @@
+from functools import wraps
+
+def val_checker(l_func):
+    def _val_checker(func):
+
+        @wraps(func)
+        def wrapper(num):
+            if l_func(num):
+                print(func(num))
+            else:
+                raise ValueError(f"wrong: {num}")
+
+        return wrapper
+
+    return _val_checker
+
+@val_checker(lambda x: x > 0)
+def calc_cube(x):
+    return x **3
+
+try:
+    a = calc_cube(5)
+    print(help(calc_cube))
+except (ValueError, TypeError) as err:
+    print(err)
